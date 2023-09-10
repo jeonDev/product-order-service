d3c61fd918b3757b2a13cca7d29c4baedf6f0625
jeonDev <jjhwhdgus0727@gmail.com>
2023-09-10 21:04:02 +0900

    RestAssured 추가

---
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -0,0 +1,42 @@
+package com.example.productorderservice.product;
+
+import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+public class ProductApiTest extends ApiTest {
+
+    @Test
+    void 상품등록() {
+        final AddProductRequest request = 상품등록요청_생성();
+
+        // API 요청
+        ExtractableResponse<Response> response = RestAssured.given().log().all()  // 요청을 보내는 로그를 남김
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private AddProductRequest 상품등록요청_생성() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        return new AddProductRequest(name, price, discountPolicy);
+    }
+
+
+}
