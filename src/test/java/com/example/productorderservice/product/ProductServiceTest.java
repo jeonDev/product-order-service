ab29a529b55bcf30547b2335a7c612adaed915aa
jeonDev <jjhwhdgus0727@gmail.com>
2023-09-10 20:19:56 +0900

    Spring Bean으로 등록

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -2,20 +2,14 @@ package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 
+@SpringBootTest
 public class ProductServiceTest {
-
+    @Autowired
     private ProductService productservice;
-    private ProductPort productPort;
-    private ProductRepository productRepository;
-
-    @BeforeEach
-    void setUp() {
-        productRepository = new ProductRepository();
-        productPort = new ProductAdapter(productRepository);
-        productservice = new ProductService(productPort);
-    }
 
     @Test
     void 상품등록() {
